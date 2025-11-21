4b2041ce02e5753d6f0c0cad73ef6ccd546053d8
andreysk0304 <andreysk0304@yandex.ru>
2025-11-21 14:42:14 +0300

    dix auth check component

--- app/handlers/components/authorization_component.py
+++ app/handlers/components/authorization_component.py
@@ -2,10 +2,11 @@ import datetime
 
 from app.database.database_manager import session_maker
 
-from sqlalchemy import text
+from sqlalchemy import select
 
-from fastapi import HTTPException, status, Request
+from fastapi import Request
 
+from app.database.table_models import AccessTokens
 from app.handlers.components.responses_component import ResponsesComponent
 
 
@@ -14,24 +15,15 @@ class AuthorizationComponent:
     @staticmethod
     async def get_user_id(request: Request) -> dict | None:
         auth_header = request.headers.get("Authorization", "")
-        
-        # Проверка формата токена (Bearer <token>)
-        if not auth_header.startswith("Bearer "):
-            ResponsesComponent.response_401_error()
-        
+
         access_token = auth_header.replace("Bearer ", "").strip()
 
         if not access_token:
             ResponsesComponent.response_401_error()
 
         async with session_maker() as session:
-
-            user_with_token = await session.execute(
-                text('''SELECT user_id, created_at FROM access_tokens WHERE access_token = :access_token'''),
-                {'access_token': access_token}
-            )
-
-            user_with_token = user_with_token.fetchone()
+            user_with_token = await session.execute(select(AccessTokens).where(AccessTokens.access_token == access_token))
+            user_with_token = user_with_token.scalars().first()
 
             if user_with_token is None:
                 ResponsesComponent.response_401_error()
